5a48021668d44f8074ebb8b0bed7e28ff9823c74
Unknown <leo.meyniel@gmail.com>
2018-03-12 23:33:56 +0100

    no change and config raspberry pi 3

--- client/src/app/services/feathers.service.ts
+++ client/src/app/services/feathers.service.ts
@@ -7,13 +7,15 @@ import feathers from '@feathersjs/feathers';
 import feathersSocketIOClient from '@feathersjs/socketio-client';
 import feathersAuthClient from '@feathersjs/authentication-client';
 
+import { environment } from "../../environments/environment";
+
 /**
  * Simple wrapper for feathers
  */
 @Injectable()
 export class Feathers {
   private _feathers = feathers();                     // init socket.io
-  private _socket = io('http://localhost:3030');      // init feathers
+  private _socket = io(environment.url);      // init feathers
   constructor() {
     this._feathers
       .configure(feathersSocketIOClient(this._socket))  // add socket.io plugin
