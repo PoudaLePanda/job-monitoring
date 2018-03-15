39641bbdcb7a3fee6321d8643e3ecf5f958a64d7
Meyniel <leo.meyniel@feuilleafeuille.fr>
2018-03-15 17:00:11 +0100

    fdsv

---
+++ client/src/app/components/proxy.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { paneltop, panelbottom, panelcenter } from '../router.animations';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-proxy',
+  templateUrl: './proxy.component.html',
+  styleUrls: ['./proxy.component.scss'],
+  animations: [ paneltop, panelbottom, panelcenter ]
+})
+export class ProxyComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.createForm();
+  }
+  private createForm() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login(event) {
+    console.log(event);
+    console.log(this.loginForm.value);
+    this.authService.logIn();
+  }
+
+  getState(outlet) {
+    return outlet.activatedRouteData.state;
+  }
+}
